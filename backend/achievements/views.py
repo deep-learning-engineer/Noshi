c755a39ada8f6c5e320f9a58c3f91743807c58b5
Mikhail <mihailohrimenko300@gmail.com>
2025-06-16 00:01:44 +1000

    added achievement view

--- backend/achievements/views.py
+++ backend/achievements/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import UserAchievement
+from .serializers import UserAchievementSerializer
 
-# Create your views here.
+
+class UserAchievementsListView(generics.ListAPIView):
+    serializer_class = UserAchievementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return (UserAchievement.objects
+                .filter(user=self.request.user)
+                .select_related("achievement")
+                .order_by("-created_at"))
